450d372feeaa92733cba743a3da05701977d485b
Vadimosov22 <rct.sousniko@bsu.by>
2023-12-19 02:47:09 +0300

    add scr folder

---
+++ src/bsu/rfe/java/group5/lab2/VadimSous/varA10/Main.java
@@ -0,0 +1,13 @@
+package bsu.rfe.java.group5.lab2.VadimSous.varA10;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
+
+
